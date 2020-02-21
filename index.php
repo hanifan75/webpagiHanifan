0537a8a4bb0496cb1dd1a9f3119cce4db30b7e64
hanifan75 <hanifan75@gmail.com>
2020-02-21 19:47:04 +0700

    query

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello World";
+        print "I'm learning PHP Script Lnaguage now";
+    ?>
+</body>
+</html>
